ee530cb3e1dcb484c25bda334c57a79132236eff
jeong-ah-lim <59951720+jeong-ah-lim@users.noreply.github.com>
2023-03-28 16:41:25 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  // base: '/bts_promo/', //https://vitejs-kr.github.io/guide/static-deploy.html
   server: {
     port: 3000,
   }
